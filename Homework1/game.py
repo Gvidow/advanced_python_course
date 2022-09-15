0809bc8da788009ca84e72f1562e7d2f73850ea4
Gvidow <arzamachev.art-alex@yandex.ru>
2022-09-15 12:46:54 +0300

    added a class PlayingField

--- Homework1/game.py
+++ Homework1/game.py
@@ -1,18 +1,24 @@
-class Game:
+class PlayingField:
     def __init__(self):
-        self.pole = [[j + 3 * i for j in range(1, 4)] for i in range(3)]
-        self.num = 1
+        self.field = [[j + 3 * i for j in range(1, 4)] for i in range(3)]
 
     def get_in_line(self, ind):
-        return self.pole[ind // 3][ind % 3]
+        return self.field[ind // 3][ind % 3]
+
+    def get(self, row, col):
+        return self.field[row][col]
 
     def get_char(self, row, col):
-        c = self.pole[row][col]
+        c = self.get(row, col)
         if c == -1:
             return "X"
         elif c == -2:
             return "O"
         return str(c)
+
+    def update(self, ind, player):
+        ind -= 1
+        self.field[ind // 3][ind % 3] = -player
 
     def draw(self):
         for i in range(2):
@@ -24,10 +30,16 @@ class Game:
             print(self.get_char(2, j) + "│", end="")
         print(self.get_char(2, 2))
 
+
+class Game:
+    def __init__(self):
+        self.field = PlayingField()
+        self.num = 1
+
     def check_move(self, move):
         if type(move) != int or move > 9 or move < 1:
             return False
-        if self.get_in_line(move - 1) in (-1, -2):
+        if self.field.get_in_line(move - 1) in (-1, -2):
             return False
         return True
 
@@ -50,37 +62,37 @@ class Game:
         # -2 - Player2 win
         # -3 - no winner
         for i in (0, 3, 6):
-            if self.get_in_line(i) == self.get_in_line(i + 1) == self.get_in_line(i + 2):
-                return self.get_in_line(i)
+            if self.field.get_in_line(i) == self.field.get_in_line(i + 1) \
+                    == self.field.get_in_line(i + 2):
+                return self.field.get_in_line(i)
         for i in range(3):
-            if self.get_in_line(i) == self.get_in_line(i + 3) == self.get_in_line(i + 6):
-                return self.get_in_line(i)
-        if self.get_in_line(0) == self.get_in_line(4) == self.get_in_line(8):
-            return self.pole[0][0]
-        if self.get_in_line(2) == self.get_in_line(4) == self.get_in_line(6):
-            return self.pole[0][2]
+            if self.field.get_in_line(i) == self.field.get_in_line(i + 3) \
+                    == self.field.get_in_line(i + 6):
+                return self.field.get_in_line(i)
+        if self.field.get_in_line(0) == self.field.get_in_line(4) \
+                == self.field.get_in_line(8):
+            return self.field.get(0, 0)
+        if self.field.get_in_line(2) == self.field.get_in_line(4) \
+                == self.field.get_in_line(6):
+            return self.field.get(0, 2)
 
         for i in range(3):
             for j in range(3):
-                if self.pole[i][j] > 0:
+                if self.field.get(i, j) > 0:
                     break
             else:
                 continue
             break
         else:
             return -3
-        return None
-
-    def update(self, ind, player):
-        ind -= 1
-        self.pole[ind // 3][ind % 3] = -player
+        return 0
 
     def start(self):
         winner = 0
         while not winner:
-            self.draw()
+            self.field.draw()
             move = self.event_handler()
-            self.update(move, (self.num + 1) % 2 + 1)
+            self.field.update(move, (self.num + 1) % 2 + 1)
             winner = self.check_end_game()
             self.num += 1
         if winner == -3:
