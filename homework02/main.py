db5b48b2cc316757685652490ce47bdb225cf19a
Gvidow <arzamachev.art-alex@yandex.ru>
2022-09-24 00:26:00 +0300

    refactoring main.py

--- homework02/main.py
+++ homework02/main.py
@@ -5,9 +5,9 @@ def parse_json(json_str: str, required_fields, keywords, keyword_callback):
     if required_fields is None or keywords is None:
         return
     dictionary = json.loads(json_str)
-    for k, w in dictionary.items():
-        if k in required_fields:
-            list_words = w.split()
+    for key_word, words in dictionary.items():
+        if key_word in required_fields:
+            list_words = words.split()
             for word in list_words:
                 if word in keywords:
                     keyword_callback(word)
--- homework02/tests.py
+++ homework02/tests.py
@@ -4,15 +4,46 @@ from main import parse_json
 
 class TestParseJSON(unittest.TestCase):
     @unittest.mock.patch("main.parse_json")
-    def test_parse_json(self, mockk):
-        s = '{"animals": "cat dog duck pig", "family": "father mother soon"}'
-        parse_json(s, ["family", "sun", "d"], ["duck", "mother", "cat", "soon", "meet"], mockk)
-        # mock_calls
-        # call_args_list
+    def test_parse_json(self, callback_mock):
+        string = '{"animals": "cat dog duck pig", ' \
+                 '"family": "father mother son"}'
+        parse_json(string, ["family", "sun", "d"],
+                   ["duck", "mother", "cat", "son", "meet"], callback_mock)
+        self.assertEqual(callback_mock.call_count, 2)
+        self.assertListEqual(callback_mock.mock_calls,
+                             [unittest.mock.call("mother"),
+                              unittest.mock.call("son")])
 
-        self.assertEqual(mockk.call_count, 2)
-        self.assertListEqual(mockk.mock_calls, [unittest.mock.call("mother"),
-                                                unittest.mock.call("soon")])
+        string = """{
+        "keep": "wrong business age identify truth carry",
+        "member": "same speech help item help political",
+        "writer": "help without far leg help boy",
+        "skin": "election official wonder",
+        "threat": "necessary decide still",
+        "child": "protect without trouble here anything \
+        far board office leg impact beat both",
+        "source": ""
+        }"""
+        parse_json(string, ["source", "skin", "writer", "help", "child"],
+                   ["far", "help", "here", "both", "help", "child", "wonder"],
+                   callback_mock)
+        self.assertEqual(callback_mock.call_count, 9)
+        self.assertListEqual(callback_mock.mock_calls[2:],
+                             list(map(unittest.mock.call,
+                                      ["help", "far", "help", "wonder",
+                                       "here", "far", "both"])))
+
+        parse_json(string, ["skin", "help"], ["help"], callback_mock)
+        self.assertEqual(callback_mock.call_count, 9)
+
+        parse_json(string, [], [], callback_mock)
+        self.assertEqual(callback_mock.call_count, 9)
+
+        parse_json(string, None, None, callback_mock)
+        self.assertEqual(callback_mock.call_count, 9)
+
+    def test_with_factory(self):
+        pass
 
 
 if __name__ == "__main__":
