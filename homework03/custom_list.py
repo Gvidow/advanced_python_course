6a7bbd2014e4c313331ec22fff53a19a7d536b3e
Gvidow <arzamachev.art-alex@yandex.ru>
2022-09-29 12:29:30 +0300

    added comparison operators and the magic method __str__

--- homework03/custom_list.py
+++ homework03/custom_list.py
@@ -27,6 +27,27 @@ class CustomList(list):
     def __radd__(self, other):
         return self + other
 
+    def __eq__(self, other):
+        return sum(self) == sum(other)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        return sum(self) < sum(other)
+
+    def __le__(self, other):
+        return sum(self) <= sum(other)
+
+    def __gt__(self, other):
+        return sum(self) > sum(other)
+
+    def __ge__(self, other):
+        return sum(self) >= sum(other)
+
+    def __str__(self):
+        return f"{super().__str__()}, {sum(self)}"
+
     @staticmethod
     def custom_list(lst):
         return __class__(lst)
