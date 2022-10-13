27b2be0c6a344e5df4d85c6d3963b436e1cd3b68
Gvidow <arzamachev.art-alex@yandex.ru>
2022-10-13 14:26:09 +0300

    added filter_file.py

---
+++ 05/filter_file.py
@@ -0,0 +1,14 @@
+def filter_file(reader, words):
+    words = list(map(lambda x: x.lower(), words))
+    with open(reader, "r", encoding="utf-8") as file:
+        iterator = iter(file)
+        try:
+            while True:
+                string = next(iterator).strip().lower()
+                strings = string.split()
+                for word in words:
+                    if word in strings:
+                        yield string
+                        break
+        except StopIteration:
+            pass
