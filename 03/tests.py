8ecb4ac691116990643e756c821f24957cdccca1
Gvidow <arzamachev.art-alex@yandex.ru>
2022-11-11 01:23:38 +0300

    modification 03/tests.py

--- 03/tests.py
+++ 03/tests.py
@@ -2,6 +2,12 @@ import unittest
 from custom_list import CustomList
 
 
+def is_equal(lst1, lst2):
+    if len(lst1) != len(lst2):
+        return False
+    return all(lst1[i] == lst2[i] for i in range(len(lst1)))
+
+
 class TestCustomList(unittest.TestCase):
     def test_init(self):
         lst = [2, 4, 6]
@@ -18,8 +24,15 @@ class TestCustomList(unittest.TestCase):
     def test_add(self):
         lst1 = CustomList([1, 2, 3])
         lst2 = CustomList((6, 3, 1, 8, -3))
+        lst1_copy = lst1
+        lst2_copy = lst2
         lst3 = lst1 + lst2
-        self.assertEqual(lst3, [7, 5, 4, 8, -3])
+
+        self.assertIs(lst1, lst1_copy)
+        self.assertIs(lst2, lst2_copy)
+        self.assertTrue(is_equal(lst1, [1, 2, 3]))
+        self.assertTrue(is_equal(lst2, [6, 3, 1, 8, -3]))
+        self.assertTrue(is_equal(lst3, [7, 5, 4, 8, -3]))
         self.assertIsInstance(lst3, CustomList)
         self.assertIsNot(lst3, lst1)
         self.assertIsNot(lst3, lst2)
@@ -29,17 +42,25 @@ class TestCustomList(unittest.TestCase):
         lst4 += lst2
         self.assertIs(lst4, lst1)
 
+        # lst3 = [7, 5, 4, 8, -3]
         lst4 = [1, 2] + lst3
         lst1 = lst3 + [1, 2]
-        self.assertEqual(lst4, [8, 7, 4, 8, -3])
-        self.assertEqual(lst4, lst1)
+        self.assertTrue(is_equal(lst4, [8, 7, 4, 8, -3]))
+        self.assertTrue(is_equal(lst4, lst1))
         self.assertIsNot(lst4, lst1)
 
     def test_sub(self):
         lst1 = CustomList([10, 5])
         lst2 = CustomList((6, 3, 9, 1))
+        lst1_copy = lst1
+        lst2_copy = lst2
         lst3 = lst1 - lst2
-        self.assertEqual(lst3, [4, 2, -9, -1])
+
+        self.assertIs(lst1, lst1_copy)
+        self.assertIs(lst2, lst2_copy)
+        self.assertTrue(is_equal(lst1, [10, 5]))
+        self.assertTrue(is_equal(lst2, [6, 3, 9, 1]))
+        self.assertTrue(is_equal(lst3, [4, 2, -9, -1]))
 
         self.assertIsNot(lst3, lst1)
         self.assertIsNot(lst3, lst2)
@@ -49,10 +70,11 @@ class TestCustomList(unittest.TestCase):
         lst4 -= lst2
         self.assertIs(lst4, lst1)
 
+        # lst3 = [4, 2, -9, -1]
         lst4 = [5, 9, -6] - lst3
         lst1 = lst3 - [5, 9, -6]
-        self.assertEqual(lst4, [1, 7, 3, 1])
-        self.assertEqual(lst1, [-1, -7, -3, -1])
+        self.assertTrue(is_equal(lst4, [1, 7, 3, 1]))
+        self.assertTrue(is_equal(lst1, [-1, -7, -3, -1]))
 
         lst5 = CustomList()
         lst5 -= list(range(10))
